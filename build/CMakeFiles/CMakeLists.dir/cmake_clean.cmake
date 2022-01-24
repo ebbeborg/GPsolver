9f350ab5c65d6c5bceb634590c341963188498ad
pyramidpele <8885545+pyramidpele@users.noreply.github.com>
2022-01-24 15:45:27 +0000

    got cmake working

---
+++ build/CMakeFiles/CMakeLists.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CMakeLists.dir/main.cpp.o"
+  "CMakeLists.pdb"
+  "CMakeLists"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CMakeLists.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ebbeborg1/drive/gu/P5/Finalproject/GPsolver
+# Build directory: /home/ebbeborg1/drive/gu/P5/Finalproject/GPsolver/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
